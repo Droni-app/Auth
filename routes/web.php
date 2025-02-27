7682c979f1fa51848d75ff0b46b89471c086fd45
Gustavo Barragan <dev@droni.co>
2025-02-27 11:42:47 -0500

    Index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [WebController::class, 'index']);
