c7f93cf576f15159280b3314b5a15aa7bd6db470
Gustavo Barragan <dev@droni.co>
2025-02-27 11:30:45 -0500

    Init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('title', 'Home')
+
+@section('content')
+<div class="flex items-center justify-center h-screen bg-gray-100">
+<div class="text-center">
+    <h1 class="text-4xl font-bold text-gray-800">
+      Welcome to Laravel
+    </h1>
+    <p class="mt-4 text-lg text-gray-600">
+      This is a Laravel application with Vite.js
+    </p>
+</div>
+</div>
+@endsection
