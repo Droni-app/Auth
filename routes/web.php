55352a3d937f8801f30e0c7b8d2a6f8217061c18
Gustavo Barragan <dev@droni.co>
2025-03-01 13:34:13 -0500

    Feat: Basic Login

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
 
 Route::get('/', [WebController::class, 'index']);
